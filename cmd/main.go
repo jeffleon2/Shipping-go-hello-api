b1c33071526f589025fdc7d0a924e27f288bba2e
jeffleon2 <jeffersonleongo2@gmail.com>
2024-12-27 17:23:44 -0500

    Adding Unit Test

--- cmd/main.go
+++ cmd/main.go
@@ -1,34 +1,18 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
-)
 
-type Resp struct {
-	Language    string `json:"language"`
-	Translation string `json:"translation"`
-}
+	"github.com/jeffleon2/shipping-go-hello-api/handlers/rest"
+)
 
 func main() {
 	addr := ":8080"
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/hello",
-		func(w http.ResponseWriter, r *http.Request) {
-			enc := json.NewEncoder(w)
-			w.Header().Set("Content-Type", "application/json; charset=utf-8")
-			resp := Resp{
-				Language:    "English",
-				Translation: "Hello",
-			}
-			if err := enc.Encode(resp); err != nil {
-				panic("unable to encode response")
-			}
-		},
-	)
+	mux.HandleFunc("/hello", rest.TranslateHandler)
 
 	log.Printf("listening on %s\n", addr)
 	if err := http.ListenAndServe(addr, mux); err != nil {
